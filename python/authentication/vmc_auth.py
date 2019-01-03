15a7246705d3e6c0ae968a8bf0090f0495785544
Gordon Good <ggood@vmware.com>
2019-01-02 17:33:11 -0800

    Update VMC example to use convenience method
    The 2.4 NSX-T SDK now has a convenience method for authenticating
    to VMC. This method only requires that the caller provide the
    VMC organization ID, SDDC ID, and refresh token. The convenience
    method automatically obtains an API session token and computes
    the correct URL to access the NSX-T API. It is no longer necessary
    to add NAT rules to allow access to the NSX-T Manager. The VMC
    reverse proxy forwards the request.

--- python/authentication/vmc_auth.py
+++ python/authentication/vmc_auth.py
@@ -50,7 +50,8 @@ Services Console, choose "View My Profile", and open the
 one. Pass that as the -r (aka --refresh_token) argument to
 this example.
 
-Compatible with: NSX-T 2.1, 2.2
+Compatible with: NSX-T 2.1, 2.2, 2.3, 2.4 (however, requires
+the NSX-T 2.4 python SDK)
 
 APIs used:
 
@@ -58,63 +59,35 @@ List domains:
 GET /policy/api/v1/infra/domains
 """
 
+import argparse
 import requests
 import sys
 
 from com.vmware.nsx_policy.infra_client import Domains
-from util import getargs
+from com.vmware.nsx_policy_client_for_vmc import create_nsx_policy_client_for_vmc
 from vmware.vapi.bindings.struct import PrettyPrinter
 from vmware.vapi.lib import connect
 from vmware.vapi.security.user_password import \
     create_user_password_security_context
 from vmware.vapi.stdlib.client.factories import StubConfigurationFactory
 
-VMC_AUTH_URL = (
-    "https://console.cloud.vmware.com/csp/gateway/am/api/auth/api-tokens/authorize")
-
 
 def main():
-    # Read the command-line arguments. The args object will contain
-    # 3 properties, args.nsx_host, args.tcp_port, and args.refresh_token.
-    args = getargs.getargs()
-    if args.refresh_token is None:
-        sys.stderr.write(
-            "Error: you must provide a refresh token for this example\n")
-        sys.exit(1)
+    # Read the command-line arguments.
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-o', '--org_id', type=str, required=True,
+                            help='VMC Organization ID')
+    arg_parser.add_argument('-s', '--sddc_id', type=str, required=True,
+                            help='VMC Software Defined Datacenter ID')
+    arg_parser.add_argument('-r', '--refresh_token', type=str, required=True,
+                            help='Refresh token')
+    args = arg_parser.parse_args()
 
-    # Obtain a token to use for authenticating to the NSX Manager. We
-    # just use the python requests library directly.
-    params = {"refresh_token", args.refresh_token}
-    resp = requests.post("%s?refresh_token=%s" %
-                         (VMC_AUTH_URL, args.refresh_token))
-    resp.raise_for_status()  # Will raise exception if error
-    resp_json = resp.json()
-    # This is the access token you will pass with all NSX Manager API requests
-    access_token = resp_json["access_token"]
-
-    # Create a session using the requests library. For more information on
-    # requests, see http://docs.python-requests.org/en/master/
-    session = requests.session()
-    # Arrange for the requests library to send the bearer token header
-    # with each request.
-    session.headers["Authorization"] = "Bearer %s" % access_token
-
-    # If your NSX API server is using its default self-signed certificate,
-    # you will need the following line, otherwise the python ssl layer
-    # will reject the server certificate. THIS IS UNSAFE and you should
-    # normally verify the server certificate.
-    session.verify = False
-
-    # Set up the API connector
-    nsx_url = 'https://%s:%s' % (args.nsx_host, args.tcp_port)
-    resp = session.get("%s/api/v1/cluster" % nsx_url)
-    connector = connect.get_requests_connector(
-        session=session, msg_protocol='rest', url=nsx_url)
-    stub_config = StubConfigurationFactory.new_std_configuration(connector)
+    vmc_client = create_nsx_policy_client_for_vmc(
+        args.refresh_token, args.org_id, args.sddc_id)
 
     # Let's get a list of all Domains
-    domains_svc = Domains(stub_config)
-    domains = domains_svc.list()
+    domains = vmc_client.infra.Domains.list()
     # Create a pretty printer to make the output look nice.
     pp = PrettyPrinter()
     pp.pprint(domains)
