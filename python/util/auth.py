dfc79b6e45b8e7e67387d7f4a211901409f6bb29
Gordon Good <ggood@vmware.com>
2019-01-02 17:41:03 -0800

    Add a convenience method to create api clients
    Add methods to obtain api clients:
         create_nsx_api_client()
         create_nsx_policy_api_client()
    
    These methods return an api client object that contains all of the
    service endpoints. It is no longer necessary to create service
    endpoints individually. For example:
    
    api_client = auth.create_nsx_api_client(
        args.user, args.password, args.nsx_host, args.tcp_port,
        auth_type = auth.SESSION_AUTH)
    transport_zones = api_client.TransportZones.list()
    logical_switches = api_client.LogicalSwitches.list()

--- python/util/auth.py
+++ python/util/auth.py
@@ -41,11 +41,17 @@ POSSIBILITY OF SUCH DAMAGE.
 
 import requests
 
+from com.vmware import nsx_client
+from com.vmware import nsx_policy_client
+from vmware.vapi.bindings.stub import ApiClient
+from vmware.vapi.bindings.stub import StubFactory
 from vmware.vapi.lib import connect
 from vmware.vapi.security.user_password import \
     create_user_password_security_context
 from vmware.vapi.stdlib.client.factories import StubConfigurationFactory
 
+BASIC_AUTH = 1
+SESSION_AUTH = 2
 
 def get_basic_auth_stub_config(user, password, nsx_host, tcp_port=443):
     """
@@ -68,6 +74,12 @@ def get_basic_auth_stub_config(user, password, nsx_host, tcp_port=443):
     connector.set_security_context(security_context)
     return stub_config
 
+
+def get_basic_auth_api_client(user, password, nsx_host, tcp_port=443):
+    stub_config = get_basic_auth_stub_config(
+        user, password, nsx_host, tcp_port)
+    stub_factory = nsx_client.StubFactory(stub_config)
+    return ApiClient(stub_factory)
 
 def get_session_auth_stub_config(user, password, nsx_host, tcp_port=443):
     """
@@ -96,3 +108,34 @@ def get_session_auth_stub_config(user, password, nsx_host, tcp_port=443):
         session=session, msg_protocol='rest', url=nsx_url)
     stub_config = StubConfigurationFactory.new_std_configuration(connector)
     return stub_config
+
+
+def get_session_auth_api_client(user, password, nsx_host, tcp_port=443):
+    stub_config = get_session_auth_stub_config(
+        user, password, nsx_host, tcp_port)
+    stub_factory = nsx_client.StubFactory(stub_config)
+    return ApiClient(stub_factory)
+
+
+def create_api_client(stub_factory_class, user, password, nsx_host,
+                      tcp_port=443, auth_type=BASIC_AUTH):
+    if auth_type == BASIC_AUTH:
+        stub_config = get_basic_auth_stub_config(
+            user, password, nsx_host, tcp_port)
+    elif auth_type == SESSION_AUTH:
+        stub_config = get_session_auth_stub_config(
+            user, password, nsx_host, tcp_port)
+    stub_factory = stub_factory_class.StubFactory(stub_config)
+    return ApiClient(stub_factory)
+
+
+def create_nsx_api_client(user, password, nsx_host, tcp_port=443,
+                          auth_type=BASIC_AUTH):
+    return create_api_client(nsx_client, user, password, nsx_host,
+                             tcp_port, auth_type)
+
+
+def create_nsx_policy_api_client(user, password, nsx_host, tcp_port=443,
+                                 auth_type=BASIC_AUTH):
+    return create_api_client(nsx_policy_client, user, password, nsx_host,
+                             tcp_port, auth_type)
